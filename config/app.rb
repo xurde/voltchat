a596fddf65d94fdb6d2dde1c06327d3ed23af71c
Jorge <jorgedf@gmail.com>
2014-10-30 23:50:27 +0100

    Config for mongoHQ on Heroku.

--- config/app.rb
+++ config/app.rb
@@ -10,9 +10,12 @@ Volt.setup do |config|
 
   config.db_driver = 'mongo'
   #config.db_name = (config.app_name + '_' + Volt.env.to_s)
-  config.db_name = 'app31108145'
-  config.db_user = 'fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w'
+  #config.db_name = 'app31108145'
+  config.db_uri = 'mongodb://heroku:fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w@dogen.mongohq.com:10005/app31108145'
   config.db_host = 'dogen.mongohq.com'
+  config.db_path = ''
+  # config.db_user = 'heroku'
+  # config.db_password = 'fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w'
   config.db_port = 10005
 
   # Compression options
