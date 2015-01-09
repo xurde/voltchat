6bdca40c73591da602e1e4b26c22275d20a118c2
Jorge Díaz <jorgedf@gmail.com>
2015-01-09 04:58:23 +0100

    Using dual config develoment/heroku environments.

--- config/app.rb
+++ config/app.rb
@@ -7,16 +7,17 @@ Volt.setup do |config|
 
   # Database config all start with db_ and can be set either in the config
   # file or with an environment variable (DB_NAME for example).
+  puts "Loading config for Volt environment: #{Volt.env}"
 
-  config.db_driver = 'mongo'
-  #config.db_name = (config.app_name + '_' + Volt.env.to_s)
-  #config.db_name = 'app31108145'
-  config.db_uri = 'mongodb://heroku:fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w@dogen.mongohq.com:10005/app31108145'
-  config.db_host = 'dogen.mongohq.com'
-  config.db_path = ''
-  # config.db_user = 'heroku'
-  # config.db_password = 'fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w'
-  config.db_port = 10005
+  case Volt.env
+  when 'development'
+    config.db_driver = 'mongo'
+    config.db_host = 'localhost'
+    config.db_port = 27017
+    config.db_name = (config.app_name + '_' + Volt.env.to_s)
+  when 'heroku'
+    config.db_uri = 'mongodb://heroku:fpWrptFHDIpoUc69gbIpfjGf_B_FsyilGO2fFVEPoSpLm20P50pTKRZexpNeC7mIEWOKRQ7Vb1Oh2fKwB4UH5w@dogen.mongohq.com:10005/app31108145'
+  end
 
   # Compression options
 
